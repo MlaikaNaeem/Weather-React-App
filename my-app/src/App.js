4519b104702a9cc023b1a0c22530f00b28cb88a7
MlaikaNaeem <mlaikanaeem123@gmail.com>
2023-02-25 21:29:05 +0500

    added a search bar and css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 
-
 export default function App() {
   return (
-    <div className="weather-app">
-      <header className="App-header">
-        <div className="search-bar">
-          <form>
-            <input
-              type="text"
-              autocomplete="off"
-              placeholder="Enter a city..."
-            />
-          </form>
-        </div>
-      </header>
+    <div className="container">
+      <div className="weather-app">
+        <form>
+          <input
+            type="text"
+            autocomplete="off"
+            placeholder="Enter a city..."
+            className="search-bar"
+          />
+          <input type="submit" value="Search" className="search-btn" />
+        </form>
+      </div>
     </div>
   );
 }
-
-
