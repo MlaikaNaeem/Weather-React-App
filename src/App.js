0380cff7e87108668a712240a004e5424a326a48
MlaikaNaeem <mlaikanaeem123@gmail.com>
2023-03-03 17:07:35 +0500

    API Integration

--- src/App.js
+++ src/App.js
@@ -4,29 +4,7 @@ import WeatherDetail from "./WeatherDetail";
 export default function App() {
   return (
     <div className="container">
-      <div className="weather-app">
-        <form>
-          <div className="row mb-3 mt-3">
-            <div className="col-8 text-center">
-              <input
-                type="text"
-                autoComplete="false"
-                placeholder="Enter a city..."
-                className="search-bar"
-              />
-            </div>
-            <div className="col-4 text-center">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-outline-dark btn-lg"
-              />
-            </div>
-          </div>
-        </form>
-
-        <WeatherDetail />
-      </div>
+      <WeatherDetail />
       <div className="github-link mb-2">
         <a
           href="https://github.com/MlaikaNaeem/Weather-React-App"
--- src/WeatherDetail.js
+++ src/WeatherDetail.js
@@ -1,36 +1,92 @@
 import "./WeatherDetail.css";
+import axios from "axios";
+import { useState } from "react";
 
 export default function WeatherDetail() {
-  return (
-    <div className="row description">
-      <div className="col-7 text-center mt-4">
-        <h1>London</h1>
-        <small> Tuesday, 8:53 </small> /<small>Overcast Clouds</small>
-        <br />
-        <div>
-          <span className="main-temp">17</span>
-          <span className="units">
-            <a href="/">°C</a>
-            <span> |</span>
-            <a href="/">°F</a>
-          </span>
-          <br />
-          <img src="/" className="weather-icon" alt="weathericon" />
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      feelslike: response.data.temperature.feels_like,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      iconUrl: response.data.condition.icon_url,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="weather-app">
+        <form>
+          <div className="row mb-3 mt-3">
+            <div className="col-8 text-center">
+              <input
+                type="text"
+                autoComplete="false"
+                placeholder="Enter a city..."
+                className="search-bar"
+              />
+            </div>
+            <div className="col-4 text-center">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-dark btn-lg"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="row description">
+          <div className="col-7 text-center mt-4">
+            <h1>{weatherData.city}</h1>
+            <small> Tuesday, 8:53 </small> /<small>Overcast Clouds</small>
+            <br />
+            <div>
+              <span className="main-temp">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="units">
+                <a href="/">°C</a>
+                <span> |</span>
+                <a href="/">°F</a>
+              </span>
+              <br />
+              <img
+                src={weatherData.iconUrl}
+                className="weather-icon"
+                alt="weathericon"
+              />
+            </div>
+          </div>
+          <div className="col-5 text-center mt-4">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                Feels Like: <span className="list"></span>
+                {Math.round(weatherData.feelslike)}°
+              </li>
+              <li className="list-group-item">
+                Humidity: <span className="list"></span>
+                {weatherData.humidity}%
+              </li>
+              <li className="list-group-item">
+                Wind: <span className="list"></span>
+                {Math.round(weatherData.wind)}km/h
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="col-5 text-center mt-5">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            Feels Like: <span className="list"></span>°
-          </li>
-          <li className="list-group-item">
-            Humidity: <span className="list"></span>%
-          </li>
-          <li className="list-group-item">
-            Wind: <span className="list"></span>km/h
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "8e4abct0a56ee8b9ed8700oa801393f4";
+    let city = "London";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`;
+    axios.get(apiUrl).then(handleResponse);
+    return <div>Loading....</div>;
+  }
 }
