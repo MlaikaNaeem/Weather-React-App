15d984a7eac1fd06cd8f95f6497d1141e199acdb
MlaikaNaeem <mlaikanaeem123@gmail.com>
2023-03-28 19:35:30 +0500

    useEffect /made the forecast to change

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Forecast.css";
 import DayForecast from "./DayForecast";
@@ -7,7 +7,11 @@ export default function Forecast(props) {
   const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState(null);
 
-  function handleResponse(response) {
+useEffect(()=>{
+setLoaded(false);
+},[props.coordinates]);
+
+function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
